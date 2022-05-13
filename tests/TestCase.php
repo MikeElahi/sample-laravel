2ad14128dad89026c0c5f5e09c0d4d6dc490a082
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-13 19:00:41 +0430

    tests: use implementation of UserFixture in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,22 @@
 
 namespace WiGeeky\Todo\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as Orchestra;
+use WiGeeky\Todo\Tests\Fixture\UserFixture;
+use WiGeeky\Todo\Todo;
 use WiGeeky\Todo\TodoServiceProvider;
 
 class TestCase extends Orchestra
 {
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Todo::useAuthModel(UserFixture::class);
+    }
+
     protected function getPackageProviders($app): array
     {
         return [TodoServiceProvider::class];
