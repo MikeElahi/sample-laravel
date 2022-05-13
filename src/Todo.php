3ab6ac6b5ee3c6c41cb7f403db3f457f757b2f60
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-13 18:57:23 +0430

    tests: add Todo class to use dynamic user model

---
+++ src/Todo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WiGeeky\Todo;
+
+class Todo
+{
+    // This static variable allows for swapping the user model during testing
+    public static $authModel = "App\\Models\\User";
+
+    public static function useAuthModel($authModel) : void
+    {
+        self::$authModel = $authModel;
+    }
+}
