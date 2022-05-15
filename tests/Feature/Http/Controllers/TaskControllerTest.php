5fa7aa81e3d3536be0ae99e17fc231256d5e4392
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-15 14:16:28 +0430

    tests: expand it_can_get_list_of_tasks_with_labels

--- tests/Feature/Http/Controllers/TaskControllerTest.php
+++ tests/Feature/Http/Controllers/TaskControllerTest.php
@@ -3,6 +3,7 @@
 namespace WiGeeky\Todo\Tests\Feature\Http\Controllers;
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use WiGeeky\Todo\Models\Task;
 use WiGeeky\Todo\Tests\Feature\FeatureTestCase;
 
 class TaskControllerTest extends FeatureTestCase
@@ -16,8 +17,14 @@ class TaskControllerTest extends FeatureTestCase
      */
     public function it_can_get_list_of_tasks_with_labels()
     {
-        $response = $this->actingAs($this->createUser())->getJson('/api/tasks');
+        $user = $this->createUser();
+        $user->tasks()->createMany(
+            factory(Task::class)->times(5)->make()->toArray()
+        );
+
+        $response = $this->actingAs($user)->getJson('/api/tasks');
         $response->assertOk();
+        $response->assertJsonCount(5, 'data');
         $response->assertJsonStructure([
             'data' => [
               'id',
