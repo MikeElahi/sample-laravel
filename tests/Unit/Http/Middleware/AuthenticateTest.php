f11f4ee552f4fdb054e80c6e4fa2fa1e444abfe2
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-16 00:18:13 +0430

    refactor: move AuthenticateTest to UnitTests

--- tests/Unit/Http/Middleware/AuthenticateTest.php
+++ tests/Unit/Http/Middleware/AuthenticateTest.php
@@ -1,75 +1,60 @@
 <?php
 
-namespace WiGeeky\Todo\Tests\Feature\Http\Middlewares;
+namespace WiGeeky\Todo\Tests\Unit\Http\Middleware;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
-use WiGeeky\Todo\Http\Middleware\Authenticate;
-use WiGeeky\Todo\Tests\TestCase;
 
-class AuthenticateTest extends TestCase
+class AuthenticateTest extends MiddlewareTestCase
 {
-    protected function usesAuthRoutes($app)
-    {
-        Route::get('/api/auth', function () {
-            return 'OK';
-        })->middleware(Authenticate::class);
-    }
-
     /**
-     * @define-route usesAuthRoutes
      * @test
      */
     public function it_can_authenticate_valid_user()
     {
-        $response = $this->getJson('/api/auth', [
+        $response = $this->getJson('/api/authenticate', [
             'Authorization' => "Bearer " . $this->user->token
         ]);
         $response->assertOk();
     }
 
     /**
-     * @define-route usesAuthRoutes
      * @test
      */
     public function it_can_fail_to_authenticate_with_invalid_token()
     {
-        $response = $this->getJson('/api/auth', [
+        $response = $this->getJson('/api/authenticate', [
             'Authorization' => "Bearer " . Str::random()
         ]);
         $response->assertUnauthorized();
     }
 
     /**
-     * @define-route usesAuthRoutes
      * @test
      */
     public function it_can_fail_to_authenticate_with_empty_token()
     {
         $this->createUser(['token' => '']);
-        $response = $this->getJson('/api/auth', [
+        $response = $this->getJson('/api/authenticate', [
             'Authorization' => "Bearer "
         ]);
         $response->assertUnauthorized();
     }
 
     /**
-     * @define-route usesAuthRoutes
      * @test
      */
     public function it_can_fail_to_authenticate_with_no_token()
     {
-        $response = $this->getJson('/api/auth');
+        $response = $this->getJson('/api/authenticate');
         $response->assertUnauthorized();
     }
 
     /**
-     * @define-route usesAuthRoutes
      * @test
      */
     public function it_can_fail_to_authenticate_with_invalid_header()
     {
-        $response = $this->getJson('/api/auth', [
+        $response = $this->getJson('/api/authenticate', [
             'Authorization' => Str::random()
         ]);
         $response->assertUnauthorized();
---
+++ tests/Unit/Http/Middleware/MiddlewareTestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WiGeeky\Todo\Tests\Unit\Http\Middleware;
+
+use Illuminate\Support\Facades\Route;
+use WiGeeky\Todo\Http\Middleware\Authenticate;
+use WiGeeky\Todo\Http\Middleware\Authorize;
+use WiGeeky\Todo\Tests\TestCase;
+
+class MiddlewareTestCase extends TestCase
+{
+    protected function defineRoutes($router)
+    {
+        $router->get('/api/noauth', function (){
+            return 'OK';
+        });
+
+        $router->get('/api/authenticate', function () {
+            return 'OK';
+        })->middleware(Authenticate::class);
+
+        $router->get('/api/authorize/{task}', function (int $task) {
+            return 'OK';
+        })->middleware(Authorize::class);
+    }
+}
