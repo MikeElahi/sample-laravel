70a6268e7413f9de6307239fb591fad814227880
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-15 15:04:16 +0430

    tests: implement labels in TaskControllerTest

--- tests/Feature/Http/Controllers/TaskControllerTest.php
+++ tests/Feature/Http/Controllers/TaskControllerTest.php
@@ -3,6 +3,7 @@
 namespace WiGeeky\Todo\Tests\Feature\Http\Controllers;
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use WiGeeky\Todo\Models\Label;
 use WiGeeky\Todo\Models\Task;
 use WiGeeky\Todo\Tests\Feature\FeatureTestCase;
 
@@ -17,12 +18,18 @@ class TaskControllerTest extends FeatureTestCase
      */
     public function it_can_get_list_of_tasks_with_labels()
     {
+        // Prepare
         $user = $this->createUser();
         $user->tasks()->createMany(
             factory(Task::class)->times(5)->make()->toArray()
         );
+        $user->tasks()->each(function ($task) {
+           $task->labels()->attach(factory(Label::class)->create());
+        });
 
+        // Execute
         $response = $this->actingAs($user)->getJson('/api/tasks');
+        // Assert
         $response->assertOk();
         $response->assertJsonCount(5, 'data');
         $response->assertJsonStructure([
