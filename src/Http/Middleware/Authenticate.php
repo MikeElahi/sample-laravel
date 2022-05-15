96ded46a0b72b445cb07798c221bcadb53759fd7
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-15 13:23:50 +0430

    fix: automatically mark the user as logged-in in Authenticate middleware

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -5,6 +5,7 @@ namespace WiGeeky\Todo\Http\Middleware;
 use Closure;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use WiGeeky\Todo\Todo;
 
 class Authenticate
@@ -22,11 +23,13 @@ class Authenticate
         if (empty($request->bearerToken())) {
             throw new AuthenticationException;
         }
-        Todo::$authModel::query()
+        $user = Todo::$authModel::query()
             ->where('token', $request->bearerToken())
             ->firstOr(['token'], function () {
                 throw new AuthenticationException;
             });
+        // Automatically mark the user as logged in
+        Auth::login($user);
 
         return $next($request);
     }
