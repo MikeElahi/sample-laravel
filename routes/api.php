fbbcbf7654afca708f35b2daf7f6407062b29106
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-15 19:34:26 +0430

    feat: implement LabelTaskController

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use WiGeeky\Todo\Http\Controllers\LabelController;
+use WiGeeky\Todo\Http\Controllers\LabelTaskController;
 use WiGeeky\Todo\Http\Controllers\TaskController;
 
 Route::get('tasks', [TaskController::class, 'index']);
 Route::post('tasks', [TaskController::class, 'store']);
 Route::put('tasks/{task}', [TaskController::class, 'update']);
 Route::patch('tasks/{task}', [TaskController::class, 'updateStatus']);
+Route::post('tasks/{task}/labels', LabelTaskController::class);
 
 //Route::apiResource('tasks', TaskController::class);
 Route::apiResource('labels', LabelController::class);
---
+++ src/Http/Controllers/LabelTaskController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WiGeeky\Todo\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use WiGeeky\Todo\Models\Task;
+
+class LabelTaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function __invoke(int $task, Request $request)
+    {
+        Task::query()->findOrFail($task)->labels()->sync($request->input());
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+}
