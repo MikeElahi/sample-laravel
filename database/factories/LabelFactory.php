8370a63a78278c4759a04185c3ea90eb07ab62b5
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-15 15:03:49 +0430

    fix: LabelFactory returned an array instead of string

--- database/factories/LabelFactory.php
+++ database/factories/LabelFactory.php
@@ -5,6 +5,6 @@ use WiGeeky\Todo\Models\Label;
 
 $factory->define(Label::class, function (Faker $faker) {
     return [
-      'title' => $faker->unique()->words(6),
+      'title' => $faker->unique()->words(6, true),
     ];
 });
