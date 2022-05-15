de4f8cbaff7a1de974414d6f7882c0d230f118c3
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-16 00:19:29 +0430

    feat: implement Authorize middleware

---
+++ src/Http/Middleware/Authorize.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WiGeeky\Todo\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use WiGeeky\Todo\Models\Task;
+
+class Authorize
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->route()->hasParameter('task')) {
+            abort_if($request->route()->parameter('task') and
+                !Task::query()
+                    ->where('id', (int) $request->route()->parameter('task'))
+                    ->where('user_id', (int) $request->user()->id)
+                    ->exists(),
+                404);
+        }
+        return $next($request);
+    }
+}
---
+++ tests/Unit/Http/Middleware/AuthorizeMiddlewareTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WiGeeky\Todo\Tests\Unit\Http\Middleware;
+
+use WiGeeky\Todo\Tests\Support\WithTask;
+
+class AuthorizeMiddlewareTest extends MiddlewareTestCase
+{
+    use WithTask;
+
+    /** @test */
+    public function it_can_authorize_a_valid_user()
+    {
+        $task = $this->createTask();
+        $response = $this
+            ->actingAs($this->user)
+            ->getJson('/api/authorize/' . $task->id);
+
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function it_can_deny_an_invalid_user()
+    {
+        $task = $this->createTask();
+        $response = $this
+            ->actingAs($this->createUser())
+            ->getJson('/api/authorize/' . $task->id);
+
+        $response->assertNotFound();
+    }
+
+    public function it_does_not_interfere_with_normal_routes()
+    {
+        $response = $this->actingAs($this->user)->getJson('/api/noauth');
+
+        $response->assertOk();
+    }
+}
