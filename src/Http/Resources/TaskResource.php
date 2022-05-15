644b5b57cd1044ffef4035d497e109a130f98d40
Mohammad Hossein Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-05-15 15:04:32 +0430

    feat: create TaskResource

---
+++ src/Http/Resources/TaskResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WiGeeky\Todo\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'title' => $this->resource->title,
+            'description' => $this->resource->description,
+            'labels' => LabelResource::collection($this->whenLoaded('labels')),
+        ];
+    }
+}
